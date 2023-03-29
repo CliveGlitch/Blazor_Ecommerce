b0bd571a3249d395360eba3fb7da1fa3f30d035f
Enrique <enriqueachavez@hotmail.com>
2023-03-29 17:51:45 -0600

    Product List CSS upgrade and new packages installed
    The style of the product list view was updated.
    Entity Framework and Swagger NuGet Packages  installed.

--- Blazor_Ecommerce/Client/Program.cs
+++ Blazor_Ecommerce/Client/Program.cs
@@ -1,3 +1,4 @@
+global using Blazor_Ecommerce.Shared;
 using Blazor_Ecommerce.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
---
+++ Blazor_Ecommerce/Client/Shared/ProductList.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace Blazor_Ecommerce.Client.Shared
+{
+    public partial class ProductList : ComponentBase
+    {
+        private static List<Product> Products = new List<Product>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            var result = await Http.GetFromJsonAsync<List<Product>>("api/Product");
+            if(result is not null)
+                Products = result;
+        }
+    }
+}
